2c8c0e9cb701e236c4ed12272db7f8d4ddd3a0dc
rish-16 <mail.rishabh.anand@gmail.com>
2020-06-08 00:42:16 +0800

    Add is_full function; update README on running instructions

--- src/minimax.c
+++ src/minimax.c
@@ -12,12 +12,26 @@ void display(char * pos[3][3], int length) {
 	}
 }
 
+bool is_full(char * pos[3][3], int length) {
+	for (int i = 0; i < length; i++) {
+		for (int j = 0; j < length; j++) {
+			if (strncmp(pos[i][j], ".", 1) == 1) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 int main() {
 	char * ai = "X";
 	char * human = "O";
 	
 	char * board[3][3] = {{".", ".", "."}, {".", ".", "."}, {".", ".", "."}};
-	int board_size = sizeof (board) / sizeof (board[0]);
+	int board_size = sizeof (board) / sizeof (board[0]); // 3
 	
 	display(board, board_size);
+	bool board_full = is_full(board, board_size);
+	
+	printf (board_full ? "true\n" : "false\n");
 }
